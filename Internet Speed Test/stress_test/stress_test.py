87ebea4e831ab97537ae0fda69b59eb005c2abb7
Matt Dassow <dassowmd@gmail.com>
2020-08-23 20:33:40 -0500

    fix internet speed stress test multithreading

--- Internet Speed Test/stress_test/stress_test.py
+++ Internet Speed Test/stress_test/stress_test.py
@@ -3,24 +3,43 @@ import datetime
 import time
 from multiprocessing.pool import ThreadPool
 
-thread_count = 1
-pool = ThreadPool(thread_count)
 
-def get_url(url=r'http://cachefly.cachefly.net/100mb.test'):
-    total_time = 0
+def update_stats(start_time):
+    global total_time
+    global count
+    end_time = datetime.datetime.now()
+    print(f'Download Time: {end_time - start_time}')
+    total_time += (end_time - start_time).total_seconds()
+    print(f'Average Download Time: {total_time / (count)}')
+    count += 1
+
+
+def get_url():
+    start_time = datetime.datetime.now()
+    response = requests.get(r'http://cachefly.cachefly.net/100mb.test')
+    return start_time
+
+def loop_get_url():
+    sub_pool = ThreadPool(1)
     loop_count = 1
     while loop_count <= 180:
-        start_time = datetime.datetime.now()
-        response = requests.get(url)
-        end_time = datetime.datetime.now()
-        print(f'Download Time: {end_time - start_time}')
-        total_time += (end_time - start_time).total_seconds()
-        print(f'Average Download Time: {total_time / (loop_count * thread_count)}')
+        sub_pool.apply_async(get_url, callback=update_stats)
         loop_count += 1
-        time.sleep(60)
+        time.sleep(0)
+    sub_pool.close()
+    sub_pool.join()
+
+
+thread_count = 5
+pool = ThreadPool(thread_count)
+count = 1
+total_time = 0
 
 for i in range(thread_count):
-    pool.apply_async(get_url)
+    pool.apply_async(loop_get_url)
+    time.sleep(4)
 
 pool.close()
 pool.join()
+
+print(f'count: {count}')
